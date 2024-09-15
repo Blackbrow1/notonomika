b3bb2c3e580d0d5613d33a5bcb3a14f2dc8ca27d
Александр Чернобров <alesandro83@mail.ru>
2024-09-15 16:16:43 +0300

    Добавил правила в htaccess

--- src/mail.php
+++ src/mail.php
@@ -1,6 +1,8 @@
 <?php
 
-$mail_to = 'info@notonomika.ru';
+$mail_to_1 = 'info@notonomika.ru';
+$mail_to_2 = 'a.blackbrow@yandex.ru';
+$mail_to_3 = 'elena.tomazi@yandex.ru';
 $email_from = 'info@notonomika.ru';
 $name_from = 'Онлайн менеджер';
 $subject = 'Заявка с сайта';
@@ -16,8 +18,10 @@ $headers = "MIME-Version: 1.0" . PHP_EOL .
     "From: " . "=?utf-8?B?" . base64_encode($name_from) . "?=" . "<" . $email_from . ">" .  PHP_EOL .
     "Reply-To: " . $email_from . PHP_EOL;
 
-$mailResult = mail($mail_to, $subject, $message, $headers);
+$mailResult1 = mail($mail_to_1, $subject, $message, $headers);
+$mailResult2 = mail($mail_to_2, $subject, $message, $headers);
+$mailResult3 = mail($mail_to_3, $subject, $message, $headers);
 
-if ($mailResult) {
+if ($mailResult1 && $mailResult2 && $mailResult3) {
   header('location: thanks');
 }
